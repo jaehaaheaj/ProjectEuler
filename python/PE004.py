f501b374a34d4375ee49825fcb43d2b5b4597acc
jaehaaheaj <jaehaaheaj@gmail.com>
2017-04-18 17:36:52 +0900

    solved PE004 (naive)

--- python/PE004.py
+++ python/PE004.py
@@ -4,5 +4,15 @@ def IsPalidrome(num):
 	else:
 		return False
 
-print(IsPalidrome(12345))
-print(IsPalidrome(123454321))
+#print(IsPalidrome(12345))
+#print(IsPalidrome(123454321))
+
+limit = 999
+ans = 0
+for i in range(limit, 100, -1):
+	for j in range(limit, i-1, -1):
+		if IsPalidrome(i*j) and i*j > ans:
+			ans = i * j
+
+print(ans)
+
